ee7fac4692b65aaa8d267d7485848f97ac5e789c
Mark Lawson <mark@walkonmars.co.uk>
2022-09-06 20:01:02 +0100

    deleted dummy images

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,55 +1,19 @@
 import React from "react";
 import "./Projects.scss";
 
-import a from "../../assets/images/projectA.png";
-import b from "../../assets/images/projectB.png";
-import c from "../../assets/images/projectC.png";
-import d from "../../assets/images/projectD.png";
-
 const Projects = ({id, title, shortDesc, longDesc, image, link}) => {
+  console.log(image, title, shortDesc, longDesc, link);
+
   return (
     <>
       <div className="projects">
-        {/* Project item 1 - FEATURED */}
-        {/* <figure className="projects__item featured">
-          <img src={strImage} alt="featured portfolio item" />
-          <figcaption>
-            <h3 className="projects__title">{strTitle}</h3>
-            <p className="projects__desc">{strShortDesc}</p>
-          </figcaption>
-        </figure> */}
-
-        <figure className="projects__item">
-          <img src={image} alt=" portfolio item" />
-          <figcaption>
-            <h3 className="projects__title">{title}</h3>
-            <p className="projects__desc">{shortDesc}</p>
-          </figcaption>
-        </figure>
-
-        {/* <figure className="projects__item">
-          <img src={b} alt=" portfolio item" />
-          <figcaption>
-            <h3 className="projects__title"> Project B </h3>
-            <p className="projects__desc">Short description goes here.</p>
-          </figcaption>
-        </figure>
-
-        <figure className="projects__item">
-          <img src={c} alt=" portfolio item" />
-          <figcaption>
-            <h3 className="projects__title"> Project C </h3>
-            <p className="projects__desc">Short description goes here.</p>
-          </figcaption>
-        </figure>
-
-        <figure className="projects__item">
-          <img src={d} alt=" portfolio item" />
-          <figcaption>
-            <h3 className="projects__title"> Project D </h3>
-            <p className="projects__desc">Short description goes here.</p>
-          </figcaption>
-        </figure> */}
+        <div className="projects-item">
+          <img src={image} alt={shortDesc} />
+          <div className="projects-item__content">
+            <h3 className="projects-item__title">{title}</h3>
+            <p className="projects-item__desc">{shortDesc}</p>
+          </div>
+        </div>
       </div>
     </>
   );
