30d870e6a04a3ec39a1713f8013af1f4fde96d7f
Mark Lawson <mark@walkonmars.co.uk>
2022-09-08 20:32:45 +0100

    working on project modal

--- portfolio/src/components/ProjectModal/ProjectModal.jsx
+++ portfolio/src/components/ProjectModal/ProjectModal.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./ProjectModal.scss";
+import {disableBodyScroll, enableBodyScroll} from "body-scroll-lock";
 
-const ProjectModal = () => {
-  return <div>ProjectModal</div>;
+const ProjectModal = ({closeModal, showModal, image, title, longDesc, stack, shortDesc}) => {
+  showModal ? disableBodyScroll(document) : enableBodyScroll(document);
+  const overlay = "project-modal__overlay";
+
+  return (
+    <>
+      {/* <div className={showModal ? overlay : "hide"} onClick={closeModal} /> */}
+      <div className={showModal ? "project-modal" : "project-modal"} onClick={closeModal}>
+        <button className="close" onClick={closeModal}>
+          &times;
+        </button>
+        <div className="project-modal__top-section">
+          <h2 className="project-modal__top-section-title">{title}</h2>
+        </div>
+
+        <div className="project-modal__content">
+          <h3 className="project-modal__short-desc">{shortDesc}</h3>
+
+          <p className="project-modal__long-desc">{longDesc}</p>
+          <ul>
+            <li>{stack[0]}</li>
+            <li>{stack[1]}</li>
+            <li>{stack[2]}</li>
+            <li>{stack[3]}</li>
+          </ul>
+          {/* <img className="project-modal__top-section__image" src={image} alt={shortDesc} /> */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectModal;
--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import {React, useState} from "react";
 import "./Projects.scss";
+import ProjectModal from "../ProjectModal/ProjectModal";
+import {CSSTransition} from "react-transition-group";
 
 const Projects = ({id, title, shortDesc, longDesc, image, link, stack}) => {
   const [showModal, setShowModal] = useState(false);
@@ -7,21 +9,38 @@ const Projects = ({id, title, shortDesc, longDesc, image, link, stack}) => {
   const closeModal = () => setShowModal(false);
 
   return (
-    <div className="projects" id={id}>
-      <div className="projects-item">
-        <img src={image} alt={shortDesc} />
-        <div className="projects-item__content">
-          <h3 className="projects-item__title">{title}</h3>
-          <p className="projects-item__desc">{shortDesc}</p>
-          <p className="projects-item__desc">{stack}</p>
-          {!showModal && (
+    <>
+      <div className="projects" id={id}>
+        <div className="projects-item">
+          <img src={image} alt={shortDesc} />
+
+          <div className="projects-item__content">
+            <h3 className="projects-item__title">{title}</h3>
+
+            <p className="projects-item__desc">{stack}</p>
+            {/* {!showModal && ( */}
             <button className="projects-item__btn" onClick={openModal}>
               View Project
             </button>
-          )}
+            {/* )} */}
+
+            <CSSTransition in={showModal} timeout={500} classNames="open-modal" unmountOnExit>
+              <ProjectModal
+                // prettier-ignore
+                closeModal={closeModal}
+                showModal={showModal}
+                image={image}
+                title={title}
+                longDesc={longDesc}
+                link={link}
+                shortDesc={shortDesc}
+                stack={stack}
+              />
+            </CSSTransition>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
