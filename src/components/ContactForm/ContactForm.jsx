522709bca43c5345378847a18209338dc460a0c6
Mark Lawson <mark@walkonmars.co.uk>
2022-09-14 17:35:18 +0100

    working on form validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,28 +1,104 @@
-import React from "react";
+import {useState} from "react";
 
 import "./ContactForm.scss";
 
 const ContactForm = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const enteredEmailIsValid = enteredEmail.includes("@") && enteredEmail.includes(".") && enteredEmail.trim() !== "";
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const handleNameInputChange = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const handleNameInputBlur = () => {
+    setEnteredNameTouched(true);
+  };
+
+  const handleEmailInputChange = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const handleEmailInputBlur = () => {
+    setEnteredEmailTouched(true);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
+
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+
+    setEnteredName("");
+    setEnteredNameTouched(false);
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsInvalid ? "invalid" : "valid";
+  const emailInputClasses = emailInputIsInvalid ? "invalid" : "valid";
+
   return (
     <div className="contact">
       <h2 className="contact__heading">Get in touch</h2>
-      <form action="mailto:mark@walkonmars.co.uk" method="get" encType="text/plain">
-        <label className="contact__name">
-          Name:
-          <input type="text" id="name" />
-        </label>
+      <form onSubmit={handleFormSubmit}>
+        <div className={nameInputClasses}>
+          <label className="contact__name" htmlFor="name">
+            Name:
+          </label>
+          <input
+            // prettier-ignore
+            type="text"
+            id="name"
+            onChange={handleNameInputChange}
+            onBlur={handleNameInputBlur}
+            value={enteredName}
+          />
+          {nameInputIsInvalid && <p className="contact__error">Name field must not be empty.</p>}
+        </div>
 
-        <label className="contact__email">
-          Email:
-          <input type="text" id="email" />
-        </label>
+        <div className={emailInputClasses}>
+          <label className="contact__email" htmlFor="email">
+            Email:
+          </label>
+          <input
+            // prettier-ignore
+            type="email"
+            id="email"
+            onChange={handleEmailInputChange}
+            onBlur={handleEmailInputBlur}
+            value={enteredEmail}
+          />
+          {emailInputIsInvalid && <p className="contact__error">Please enter a valid email.</p>}
+        </div>
 
         <label message="message" className="contact__message">
           Message:
           <textarea name="" id="message"></textarea>
         </label>
 
-        <input className="btn contact__btn" type="submit" value="send" />
+        <button className="btn contact__btn" type="submit" disabled={!formIsValid}>
+          Send
+        </button>
       </form>
     </div>
   );
