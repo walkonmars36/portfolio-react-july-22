b066619f4192c68c6930738370128dbf21294c47
Mark Lawson <mark@walkonmars.co.uk>
2022-10-10 16:02:12 +0100

    with fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,18 @@
-import {useState, useEffect, useRef} from "react";
+import React from "react";
 import "./Header.scss";
 
 import OpenBracket from "../OpenBracket/OpenBracket";
 import ClosingBracket from "../ClosingBracket/ClosingBracket";
 
 import Hamburger from "hamburger-react";
-import {CSSTransition} from "react-transition-group";
+
 import {disableBodyScroll, enableBodyScroll} from "body-scroll-lock";
 
 const Header = ({handleToggleNav, navOpen, setNavOpen}) => {
-  const [showHeader, setShowHeader] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-  const nodeRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > lastScrollY) {
-        setShowHeader(false);
-      } else {
-        setShowHeader(true);
-      }
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
-
   navOpen ? disableBodyScroll(document) : enableBodyScroll(document);
 
   return (
-    // <CSSTransition in={showHeader} timeout={1000} classNames="show-header" unmountOnExit nodeRef={nodeRef}>
-    <header className="header" ref={nodeRef}>
+    <header className="header">
       <div className="header__title">
         <OpenBracket />
         <h5 className="header__title-name">MarkLawson</h5>
@@ -43,7 +23,6 @@ const Header = ({handleToggleNav, navOpen, setNavOpen}) => {
         <Hamburger rounded size={25} color="#f5f5f5" label="Show menu" toggled={navOpen} toggle={setNavOpen} onToggle={handleToggleNav} />
       </span>
     </header>
-    // </CSSTransition>
   );
 };
 
