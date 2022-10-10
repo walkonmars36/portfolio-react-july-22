d2415fed415b06ef6467af6086147655645e430a
Mark Lawson <mark@walkonmars.co.uk>
2022-10-10 15:20:56 +0100

    bracket sizing

--- src/components/ClosingBracket/ClosingBracket.jsx
+++ src/components/ClosingBracket/ClosingBracket.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./ClosingBracket.scss";
 
 const ClosingBracket = () => {
   return (
-    <svg width="15" height="13" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className="closing-bracket" viewBox="0 0 15 13" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M6.73438 0.625L2.5625 12.9766H0.890625L5.0625 0.625H6.73438ZM12.635 7.28125L7.62719 5.49219V3.39062L14.4319 6.25V7.75781L12.635 7.28125ZM7.62719 8.78125L12.6428 6.95312L14.4319 6.52344V8.02344L7.62719 10.8828V8.78125Z"
         fill="#F5F5F5"
--- src/components/OpenBracket/OpenBracket.jsx
+++ src/components/OpenBracket/OpenBracket.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./OpenBracket.scss";
 
 const OpenBracket = () => {
   return (
-    <svg width="10" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className="open-bracket" viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg">
       <path d="M2.20312 3.96875L7.17188 5.76562V7.88281L0.421875 5.02344V3.52344L2.20312 3.96875ZM7.17188 2.5L2.19531 4.32812L0.421875 4.75V3.25L7.17188 0.390625V2.5Z" fill="#F5F5F5" />
     </svg>
   );
