eb1be797f57cf8efd3bfb885cebbfe5504f63f89
Mark Lawson <mark@walkonmars.co.uk>
2022-10-13 17:58:00 +0100

    completed links

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,39 +10,39 @@ const Skills = () => {
       <p>Below are the technologies I have used so far, I expect to add to these as I am always keen to learn new methods and blah blah blah! </p>
       <div className="technical-skills__logos">
         <div className="technical-skills__icon">
-          <p>Git/Github</p>
-          <FontAwesomeIcon icon={faGithubAlt} size="3x" />
+          <p className="technical-skills__icon-title">Git/Github</p>
+          <FontAwesomeIcon icon={faGithubAlt} />
         </div>
         <div className="technical-skills__icon">
-          <p>HTML 5</p>
-          <FontAwesomeIcon icon={faHtml5} size="3x" />
+          <p className="technical-skills__icon-title">HTML 5</p>
+          <FontAwesomeIcon icon={faHtml5} />
         </div>
         <div className="technical-skills__icon">
-          <p>CSS 3</p>
-          <FontAwesomeIcon icon={faCss3Alt} size="3x" />
+          <p className="technical-skills__icon-title">CSS 3</p>
+          <FontAwesomeIcon icon={faCss3Alt} />
         </div>
         <div className="technical-skills__icon">
-          <p>Javascript</p>
-          <FontAwesomeIcon icon={faJsSquare} size="3x" />
+          <p className="technical-skills__icon-title">Javascript</p>
+          <FontAwesomeIcon icon={faJsSquare} />
         </div>
         <div className="technical-skills__icon">
-          <p>SCSS/Sass</p>
-          <FontAwesomeIcon icon={faSass} size="3x" />
-        </div>
-
-        <div className="technical-skills__icon">
-          <p>React</p>
-          <FontAwesomeIcon icon={faReact} size="3x" />
+          <p className="technical-skills__icon-title">SCSS/Sass</p>
+          <FontAwesomeIcon icon={faSass} />
         </div>
 
         <div className="technical-skills__icon">
-          <p>Java</p>
-          <FontAwesomeIcon icon={faJava} size="3x" />
+          <p className="technical-skills__icon-title">React</p>
+          <FontAwesomeIcon icon={faReact} />
         </div>
 
         <div className="technical-skills__icon">
-          <p>Spring</p>
-          <FontAwesomeIcon icon={faJava} size="3x" />
+          <p className="technical-skills__icon-title">Java</p>
+          <FontAwesomeIcon icon={faJava} />
+        </div>
+
+        <div className="technical-skills__icon">
+          <p className="technical-skills__icon-title">Spring</p>
+          <FontAwesomeIcon icon={faJava} />
         </div>
       </div>
     </div>
