ef166c3eaa4bac88874e74976a9db77583cf0d0a
Mark Lawson <mark@walkonmars.co.uk>
2022-10-20 11:32:42 +0100

    added FA close icon

--- src/components/ProjectModal/ProjectModal.jsx
+++ src/components/ProjectModal/ProjectModal.jsx
@@ -5,7 +5,7 @@ import Overlay from "../Overlay/Overlay";
 import {CSSTransition} from "react-transition-group";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
+import {faArrowUpRightFromSquare, faCircleXmark} from "@fortawesome/free-solid-svg-icons";
 import {faGithubAlt} from "@fortawesome/free-brands-svg-icons";
 
 const ProjectModal = ({project, showModal, closeModal}) => {
@@ -17,12 +17,9 @@ const ProjectModal = ({project, showModal, closeModal}) => {
     <>
       {showModal && <Overlay />}
       <CSSTransition in={showModal} timeout={500} classNames="open-modal" unmountOnExit nodeRef={nodeRef}>
-        <div className="project-modal" onClick={closeModal} ref={nodeRef}>
+        <div className="project-modal" ref={nodeRef}>
+          <FontAwesomeIcon icon={faCircleXmark} className="project-modal__close" onClick={closeModal} />
           <div className="project-modal__content">
-            <button className="project-modal__content-close" onClick={closeModal}>
-              &times;
-            </button>
-
             <h3 className="project-modal__title">{strTitle}</h3>
             <h5 className="project-modal__short-desc">{strShortDesc}</h5>
 
